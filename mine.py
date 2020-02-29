9a85019d7945e91b4c9a4c1df58d878e96be16cd
Dionuta <diounta@gmail.com>
2020-02-29 15:02:39 -0800

    last block json

--- mine.py
+++ mine.py
@@ -5,19 +5,19 @@ def proof_of_work(last_block, difficulty):
 
 
     # last block to json
-    bl_string = json.dumps(last_block, sort_keys=True)
+    lb_json = json.dumps(last_block, sort_keys=True)
 
     proof = 0
 
-    while valid_proof(bl_string, proof, difficulty) is False:
+    while valid_proof(lb_json, proof, difficulty) is False:
 
         proof += 1
 
     return proof
 
-def valid_proof(bl_string, proof, difficulty):
+def valid_proof(lb_json, proof, difficulty):
 
-    guess = f"{bl_string}{proof}".encode()
+    guess = f"{lb_json}{proof}".encode()
 
     guess_hash = hashlib.sha256(guess).hexdigest()
 
